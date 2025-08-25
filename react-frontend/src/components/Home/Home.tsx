c26b8e9362662c0c21e0eea02674274e21e66d17
bduo10 <bduong0312@gmail.com>
2025-08-25 12:07:38 -0400

    delete license

--- react-frontend/src/components/Home/Home.tsx
+++ react-frontend/src/components/Home/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className="home-text">
                 <h1>Welcome to the University of Maryland Lost & Found!</h1>
                 <p>
-                    This website acts as the unofficial hub for lost and 
+                    This website acts as the <b>UNOFFICIAL</b> hub for lost and 
                     found items for the school. If you find a lost item, make a post on the 
                     feed to help the owner find it! If you're looking for your lost item, 
                     check the feed to see if someone has found it. 
